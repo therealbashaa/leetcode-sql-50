870fe35e0eaf85c294ac6a5af51f732ef04d7821
Gouse Basha <gousetough@gmail.com>
2026-07-18 14:24:11 +0530

    SQL/50/Probelm/47

---
+++ Medium/176_Second_Highest_Salary.sql
@@ -0,0 +1,70 @@
+/*
+Table: Employee
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains information about the salary of an employee.
+ 
+
+Write a solution to find the second highest distinct salary from the Employee table. If there is no second highest salary, return null (return None in Pandas).
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+Example 2:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| null                |
++---------------------+
+*/
+
+-- Write your PostgreSQL query statement below
+WITH temp AS (SELECT salary,
+DENSE_RANK() OVER(ORDER BY salary DESC) AS shigh
+FROM Employee
+)
+
+SELECT(SELECT salary
+FROM temp
+WHERE shigh = 2
+LIMIT 1
+) AS SecondHighestSalary;
+
+------OR-------------------------------
+
+SELECT MAX(salary) AS SecondHighestSalary
+FROM Employee
+WHERE salary NOT IN (SELECT MAX(salary) FROM Employee);
